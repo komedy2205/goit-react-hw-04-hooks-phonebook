aef5ba3b4036cee9a1032ef2353d40c5994171b1
komedy2205 <komedy2205@gmail.com>
2022-04-30 21:40:50 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,79 +1,14 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Contactform from './components/ContactForm/ContactForm';
 import ContactList from './components/ContactList/ContactList'
 import Filter from './components/Filter/Filter';
 import { nanoid } from 'nanoid';
 
-class Phonebook extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  }
-  
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-  
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts })
-    };
-  }
+export default function Phonebook() {
+  const [contact, useContact] = useState();
+  const [filter, useFilter] = useState();
 
-  deleteContact = (contactId) => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  addContact = (name, number) => {
-    const contact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-
-    for (const contact of this.state.contacts) {
-      if (name === contact.name) {
-        alert(`${name} is allready in contacts`)
-        return
-      }
-    }
-    
-    this.setState(prevState => ({
-      contacts: [contact, ...prevState.contacts],
-    }));
-        
-  };
-
-  formSubmitHandler = ({name,number}) => {
-    this.addContact(name,number);
-  };
-
-  changeFilter = (e) => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  getRealContacts = () => {
-    const {contacts, filter } = this.state;
-    
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter),
-    );
-
-    
-  }
-
-  render() {
-
-    const { filter } = this.state;
-    const realContacts = this.getRealContacts();
-
-    return (
+  return (
       <>
         <h1>Phonebook</h1>
         <Contactform
@@ -91,8 +26,97 @@ class Phonebook extends Component {
         />
         </>
     )
-  
-  }
 }
 
-export default Phonebook;
+// class Phonebook extends Component {
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   }
+  
+//   componentDidUpdate(prevProps, prevState) {
+//     if (this.state.contacts !== prevState.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+  
+//   componentDidMount() {
+//     const contacts = localStorage.getItem('contacts');
+//     const parsedContacts = JSON.parse(contacts);
+//     if (parsedContacts) {
+//       this.setState({ contacts: parsedContacts })
+//     };
+//   }
+
+  // deleteContact = (contactId) => {
+  //   this.setState(prevState => ({
+  //     contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+  //   }));
+  // };
+
+//   addContact = (name, number) => {
+//     const contact = {
+//       id: nanoid(),
+//       name,
+//       number,
+//     };
+
+//     for (const contact of this.state.contacts) {
+//       if (name === contact.name) {
+//         alert(`${name} is allready in contacts`)
+//         return
+//       }
+//     }
+    
+//     this.setState(prevState => ({
+//       contacts: [contact, ...prevState.contacts],
+//     }));
+        
+//   };
+
+//   formSubmitHandler = ({name,number}) => {
+//     this.addContact(name,number);
+//   };
+
+//   changeFilter = (e) => {
+//     this.setState({ filter: e.currentTarget.value });
+//   };
+
+//   getRealContacts = () => {
+//     const {contacts, filter } = this.state;
+    
+//     const normalizedFilter = filter.toLowerCase();
+//     return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter),
+//     );
+
+    
+//   }
+
+//   render() {
+
+//     const { filter } = this.state;
+//     const realContacts = this.getRealContacts();
+
+//     return (
+//       <>
+//         <h1>Phonebook</h1>
+//         <Contactform
+//           onSubmit={this.formSubmitHandler} />
+          
+//         <h2>Contacts</h2>
+//         <Filter
+//           value={filter}
+//           onChange={this.changeFilter}
+//         />
+        
+//         <ContactList
+//           option={realContacts}
+//           onDeleteContact={this.deleteContact}
+//         />
+//         </>
+//     )
+  
+//   }
+// }
+
+// export default Phonebook;
